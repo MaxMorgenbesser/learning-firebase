cd7f35a5060323d4bc0a77267953845efc6ef64d
Max Morgenbesser <maxwellmorgenbesser@gmail.com>
2022-07-22 11:50:44 -0400

    learning firebase

---
+++ index.js
@@ -0,0 +1,18 @@
+import { initializeApp, cert } from 'firebase-admin/app'
+import {getFirestore } from "firebase-admin/firestore"
+import {credentials} from './credentials.js'
+
+initializeApp({
+    credential: cert(credentials)
+})
+
+const db = getFirestore()
+
+const car = {
+    make:"BMW",
+    model:"M3",
+    year: 2020,
+    color:'Blue'
+}
+
+db.collection('cars')
